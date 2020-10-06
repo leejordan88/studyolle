0be847a145644cc5247594b013e2a1be23730a11
jordan lee <leejs198@gmail.com>
2020-10-07 01:53:22 +0900

    로그인/로그아웃 완료

--- src/main/java/com/studyolle/account/AccountRepository.java
+++ src/main/java/com/studyolle/account/AccountRepository.java
@@ -12,4 +12,6 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     boolean existsByNickname(String email);
 
     Account findByEmail(String email);
+
+    Account findByNickname(String emailOrNickName);
 }
