96ec3e169c03314043b4b41299e1af73a5dc9fa0
joonsunglee <js.lee@petdoc.co.kr>
2020-10-29 21:59:53 +0900

    패스워드 변경

---
+++ src/main/java/com/studyolle/settings/PasswordForm.java
@@ -0,0 +1,18 @@
+package com.studyolle.settings;
+
+import com.studyolle.domain.Account;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+@NoArgsConstructor
+public class PasswordForm {
+
+    @Length(min = 8, max = 50)
+    private String newPassword;
+    @Length(min = 8, max = 50)
+    private String newPasswordConfirm;
+
+
+}
