ce1f849f17700c62f4ca5cc3932e37875db00eef
jordan lee <leejs198@gmail.com>
2020-11-16 01:49:59 +0900

    스터디 등록 오류 많음...

---
+++ src/main/java/com/studyolle/study/validator/StudyFormValidator.java
@@ -0,0 +1,31 @@
+package com.studyolle.study.validator;
+
+import com.studyolle.study.StudyRepository;
+import com.studyolle.study.form.StudyForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+
+@Component
+@RequiredArgsConstructor
+public class StudyFormValidator implements Validator {
+
+    private final StudyRepository studyRepository;
+
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return StudyForm.class.isAssignableFrom(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        StudyForm studyForm = (StudyForm) o;
+        if (studyRepository.existsByPath(studyForm.getPath())) {
+            errors.rejectValue("path", "wrong.path", "스터디 경로를 사용할 수 없습니다.");
+
+        }
+    }
+}
